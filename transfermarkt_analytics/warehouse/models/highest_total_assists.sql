fa6ad05d6ec660a3df6f45b1ef6588f49b7c3dfa
ther <tranthipr30@gmail.com>
2024-04-24 00:15:26 +0700

    feat: Data ingestion from Data Lake into Data Warehouse

---
+++ transfermarkt_analytics/warehouse/models/highest_total_assists.sql
@@ -0,0 +1,7 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+select *
+from DataLake.warehouse.gold."highest_total_assists"."20240419_162416_00039_utjfp_f6ba1c34-1ace-493b-82d0-8adf7062212e"
